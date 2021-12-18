cb1a28d7eb0f63bc17d301e1d321489bf0b5e304
AndriYana44 <andri.yana349@gmail.com>
2021-12-19 00:05:21 +0700

    change host for run app

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def home():
     return render_template('index.html', flash_success=flash_success, result=result, filename=filename)
 
 if __name__ == "__main__":
-    app.run(host='192.168.43.197', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
