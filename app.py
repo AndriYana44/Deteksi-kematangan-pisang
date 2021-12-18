20c3d14fce82013822fefb941d576020d07e2f31
AndriYana44 <andri.yana349@gmail.com>
2021-12-19 00:04:09 +0700

    change allowed extension

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from werkzeug.utils import secure_filename
 from PIL import Image
 
 UPLOAD_FOLDER = './static/image_upload/'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
